762aeccff1469b316d395efe9928443713aa505b
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2023-07-28 16:39:52 -0300

    2

--- model/TelaCarrinho.java
+++ model/TelaCarrinho.java
@@ -1,107 +1,68 @@
 package model;
 
-import model.Carrinho;
-import model.Produto;
+import java.awt.EventQueue;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JComboBox;
 
 public class TelaCarrinho extends JFrame {
 
-    private JComboBox<Produto> cbProdutos;
-    private JTextField tfQuantidade;
-    private JButton btnAdicionar;
-    private JButton btnRemover;
+	private JPanel contentPane;
 
-    private Carrinho carrinho;
-    private List<Produto> produtosDisponiveis;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaCarrinho frame = new TelaCarrinho();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                TelaCarrinho telaCarrinho = new TelaCarrinho(null);
-                telaCarrinho.setVisible(true);
-            }
-        });
-    }
+	/**
+	 * Create the frame.
+	 */
+	public TelaCarrinho() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(95, 160, 152));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-    	public TelaCarrinho(Cliente cliente) {
-            carrinho = new Carrinho(cliente);
-            produtosDisponiveis = new ArrayList<>();
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Victor Store");
+		lblNewLabel.setFont(new Font("Tw Cen MT Condensed", Font.PLAIN, 34));
+		lblNewLabel.setBounds(161, 11, 119, 31);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Produto");
+		lblNewLabel_1.setBounds(10, 90, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(77, 86, 65, 22);
+		contentPane.add(comboBox);
+		
+		JLabel lblNewLabel_2 = new JLabel("Quantidade");
+		lblNewLabel_2.setBounds(272, 90, 56, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(359, 86, 65, 22);
+		contentPane.add(comboBox_1);
+	}
 
-            setTitle("Cadastro de Produtos no Carrinho");
-            setSize(400, 250);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-        JPanel contentPane = new JPanel();
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
-
-        JLabel lblProduto = new JLabel("Produto:");
-        lblProduto.setBounds(20, 20, 80, 20);
-        contentPane.add(lblProduto);
-
-        cbProdutos = new JComboBox<>();
-        cbProdutos.setBounds(100, 20, 200, 20);
-        contentPane.add(cbProdutos);
-
-        JLabel lblQuantidade = new JLabel("Quantidade:");
-        lblQuantidade.setBounds(20, 50, 80, 20);
-        contentPane.add(lblQuantidade);
-
-        tfQuantidade = new JTextField();
-        tfQuantidade.setBounds(100, 50, 100, 20);
-        contentPane.add(tfQuantidade);
-
-        btnAdicionar = new JButton("Adicionar ao Carrinho");
-        btnAdicionar.setBounds(60, 100, 180, 25);
-        contentPane.add(btnAdicionar);
-
-        btnAdicionar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                adicionarProdutoAoCarrinho();
-            }
-        });
-
-        btnRemover = new JButton("Remover do Carrinho");
-        btnRemover.setBounds(240, 100, 180, 25);
-        contentPane.add(btnRemover);
-
-        btnRemover.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                removerProdutoDoCarrinho();
-            }
-        });
-    }
-
-    private void adicionarProdutoAoCarrinho() {
-        Produto produtoSelecionado = (Produto) cbProdutos.getSelectedItem();
-        int quantidade = Integer.parseInt(tfQuantidade.getText());
-
-        try {
-            carrinho.adicionarProduto(produtoSelecionado, quantidade);
-            JOptionPane.showMessageDialog(this, "Produto adicionado ao carrinho com sucesso!");
-        } catch (IllegalArgumentException ex) {
-            JOptionPane.showMessageDialog(this, "Erro ao adicionar produto ao carrinho: " + ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    private void removerProdutoDoCarrinho() {
-        Produto produtoSelecionado = (Produto) cbProdutos.getSelectedItem();
-        int quantidade = Integer.parseInt(tfQuantidade.getText());
-
-        try {
-            carrinho.removerProduto(produtoSelecionado, quantidade);
-            JOptionPane.showMessageDialog(this, "Produto removido do carrinho com sucesso!");
-        } catch (IllegalArgumentException ex) {
-            JOptionPane.showMessageDialog(this, "Erro ao remover produto do carrinho: " + ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
-        }
-    }
 }
